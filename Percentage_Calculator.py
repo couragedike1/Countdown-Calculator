d80e7517a75a1c9b8b098d9ce91fa22cd7dd6145
dike1755 <couragedike@gmail.com>
2022-08-25 18:19:09 +0100

    Created using Colaboratory

---
+++ Percentage_Calculator.py
@@ -0,0 +1,91 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Percentage Calculator ",
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyOhfmdNXUo306DZQvwGvMDR",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/dike1755/Countdown-Calculator/blob/main/Percentage_Calculator.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 7,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "Dw0uqfd-fjjj",
+        "outputId": "1a3a01ec-48c3-4677-9e08-b7c3ca03ceb1"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Enter the number4\n",
+            "Enter percenatage2\n",
+            "0.08\n"
+          ]
+        }
+      ],
+      "source": [
+        "#Calculator \n",
+        "x = input(\"Enter the number\")\n",
+        "a = int(x)\n",
+        "y = input(\"Enter percenatage\")\n",
+        "b = int(y)\n",
+        "z =  a * b/100\n",
+        "print(z)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "x = int(input(\"Enter the number\"))\n",
+        "y = int(input(\"Enter the number\"))\n",
+        "z = x * y/100\n",
+        "print(z)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "1KdcdFKMj2vB",
+        "outputId": "d408999d-ac73-48e1-a599-6e3b5491cacd"
+      },
+      "execution_count": 5,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Enter the number4\n",
+            "Enter the number2\n",
+            "0.08\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
